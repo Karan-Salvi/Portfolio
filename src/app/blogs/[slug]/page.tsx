3af93e4fc013ddc66798e0219f1987be50ecd45f
Karan <karansalviwork@gmail.com>
2025-06-12 01:10:27 +0530

    refactor: Improve SingleBlogPage component structure and readability

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,3 +1,76 @@
+// import { Metadata } from "next";
+// import React from "react";
+// import { getSingleBlog } from "@/utils/mdx";
+// import { redirect } from "next/navigation";
+// import Image from "next/image";
+// import { stringToDate } from "@/utils/date";
+// import { FaArrowLeft } from "react-icons/fa6";
+// import Link from "next/link";
+
+// export const metadata: Metadata = {
+//   title: "Karan Salvi",
+//   description:
+//     "Karan Salvi's portfolio website showcasing projects and skills.",
+// };
+
+// export default async function SingleBlogPage(params: {
+//   params: { slug: string };
+// }) {
+//   const { slug } = params.params;
+//   const blog = await getSingleBlog(slug);
+
+//   if (!blog) {
+//     redirect("/blogs");
+//   }
+
+//   const { content, frontmatter } = blog;
+
+//   console.log("SingleBlogPage Formatter : ", frontmatter);
+
+//   return (
+//     <div className="mt-24 bg-zinc-900 py-10 rounded-md">
+//       <div className="pros dark:prose-invert prose max-w-3xl mx-auto mt-10">
+//         <div className="flex flex-col gap-0">
+//           <div className="flex justify-between items-center">
+//             <h1 className="text-3xl font-extrabold mb-0">
+//               {frontmatter.title}
+//             </h1>
+//             <Link
+//               href={"/blogs"}
+//               className="border-2 p-1 rounded-full hover:scale-115 transition-all duration-200"
+//             >
+//               <FaArrowLeft className="text-xl" />
+//             </Link>
+//           </div>
+
+//           <div className="flex justify-start items-center gap-2 mb-0">
+//             <Image
+//               src="/images/avatar.jpg"
+//               className="rounded-full"
+//               alt="logo"
+//               width={30}
+//               height={30}
+//             />
+//             <p>
+//               {frontmatter.author} / {stringToDate(frontmatter.date)}
+//             </p>
+//           </div>
+
+//           <Image
+//             src={frontmatter.image}
+//             width={1600}
+//             height={800}
+//             className="w-full  aspect-video"
+//             alt={frontmatter.title}
+//           />
+//         </div>
+//         {content}
+//         {/* <MDXRemote source={SingleBlog} /> */}
+//       </div>
+//     </div>
+//   );
+// }
+
 import { Metadata } from "next";
 import React from "react";
 import { getSingleBlog } from "@/utils/mdx";
@@ -13,10 +86,14 @@ export const metadata: Metadata = {
     "Karan Salvi's portfolio website showcasing projects and skills.",
 };
 
-export default async function SingleBlogPage(params: {
-  params: { slug: string };
-}) {
-  const { slug } = params.params;
+type BlogPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function SingleBlogPage({ params }: BlogPageProps) {
+  const { slug } = params;
   const blog = await getSingleBlog(slug);
 
   if (!blog) {
@@ -24,8 +101,6 @@ export default async function SingleBlogPage(params: {
   }
 
   const { content, frontmatter } = blog;
-
-  console.log("SingleBlogPage Formatter : ", frontmatter);
 
   return (
     <div className="mt-24 bg-zinc-900 py-10 rounded-md">
@@ -36,7 +111,7 @@ export default async function SingleBlogPage(params: {
               {frontmatter.title}
             </h1>
             <Link
-              href={"/blogs"}
+              href="/blogs"
               className="border-2 p-1 rounded-full hover:scale-115 transition-all duration-200"
             >
               <FaArrowLeft className="text-xl" />
@@ -60,14 +135,12 @@ export default async function SingleBlogPage(params: {
             src={frontmatter.image}
             width={1600}
             height={800}
-            className="w-full  aspect-video"
+            className="w-full aspect-video"
             alt={frontmatter.title}
           />
         </div>
         {content}
-        {/* <MDXRemote source={SingleBlog} /> */}
       </div>
     </div>
   );
 }
-
