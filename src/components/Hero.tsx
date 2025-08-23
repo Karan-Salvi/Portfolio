08fc4c67bdc9f2614cd51d1c59193c78695bafd4
Karan-Salvi <karansalviwork@gmail.com>
2025-08-23 20:31:50 +0530

    fix:minor ui bug

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,6 @@ const Hero = () => {
       <div className="text-lg md:text-3xl text-center mx-auto font-normal text-neutral-600 dark:text-neutral-400">
         I am a
         <ContainerTextFlip words={words} />
-        {/* <FlipWords words={words} /> <br /> */}
       </div>
 
       {/* <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
